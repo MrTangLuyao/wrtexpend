e42475b4c2e75e044afa0de779075e8278b6db93
LouieTTTTTTTTTang <65329281+MrTangLuyao@users.noreply.github.com>
2026-02-04 15:18:39 +0800

    Update expend.sh

--- expend.sh
+++ expend.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+
 opkg update
-opkg install e2fsprogs resize2fs losetup sfdisk
+opkg install util-linux-fdisk util-linux-losetup e2fsprogs resize2fs tune2fs sfdisk kmod-loop
 
 DISK="/dev/mmcblk1"
 [ ! -b "$DISK" ] && DISK="/dev/sda"
@@ -12,11 +13,15 @@ PART="${DISK}p2"
 , +
 EOF
 
+[ -e /dev/loop0 ] || mknod /dev/loop0 b 7 0
+
 LOOP_DEV=$(/usr/sbin/losetup -f)
 /usr/sbin/losetup "$LOOP_DEV" "$PART"
+
 /usr/sbin/e2fsck -fy "$LOOP_DEV"
 /usr/sbin/resize2fs "$LOOP_DEV"
 /usr/sbin/tune2fs -m 1 "$LOOP_DEV"
+
 /usr/sbin/losetup -d "$LOOP_DEV"
 
 sync
